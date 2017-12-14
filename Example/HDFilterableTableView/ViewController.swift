7af97f9ae8797211ba3208b377be6958c799644d
Yusuke Mori <fr052123@gmail.com>
2017-12-14 16:45:55 +0900

    add code. (#4)
    add code.

--- Example/HDFilterableTableView/ViewController.swift
+++ Example/HDFilterableTableView/ViewController.swift
@@ -1,24 +1,44 @@
-//
-//  ViewController.swift
-//  HDFilterableTableView
-//
-//  Created by IgnorantCoder on 12/13/2017.
-//  Copyright (c) 2017 IgnorantCoder. All rights reserved.
-//
-
 import UIKit
+import HDFilterableTableView
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: HDFilterableTableViewController, HDFilterableTableViewDelegate {
+    private let viewModel = FruitsData()
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.dataSource = self.viewModel
+        self.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillLayoutSubviews()
+    {
+        super.viewWillLayoutSubviews()
+        
+        self.view.frame = UIScreen.main.bounds
+        return
     }
-
+    
+    func tableView(didSelectRowAt indexPath: IndexPath,
+                   from viewModel: HDFilterableTableViewDataSource)
+    {
+        if self.viewModel !== viewModel {
+            return  //do nothing.
+        }
+        
+        let alert = UIAlertController(title: nil, 
+                                      message: "Selected " + self.viewModel.getElement(indexPath: indexPath),
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Close",
+                                      style: .default,
+                                      handler: { e in self.deselect()}))
+        self.present(alert,
+                     animated: true,
+                     completion: nil)
+    }
 }
 
---
+++ HDFilterableTableView/Classes/HDFilterableTableViewController.swift
@@ -0,0 +1,116 @@
+import UIKit
+
+/**
+ Fileterable table view controller.
+ This controller already have search bar and table.
+ */
+open class HDFilterableTableViewController: UIViewController, UITableViewDelegate, UISearchBarDelegate, UITableViewDataSource, TableViewCellFactory {
+    //MARK:- Properties
+    /**
+     You can set height of search bar through this property. (default: 45)
+     */
+    var searchBarHeight: CGFloat = 45
+
+    open weak var delegate: HDFilterableTableViewDelegate?
+    open weak var dataSource: HDFilterableTableViewDataSource?
+    
+    //MARK:- Sub components
+    private lazy var searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        bar.delegate = self
+        return bar
+    }()
+    
+    private lazy var table: UITableView = {
+        let view = UITableView()
+        view.register(UITableViewCell.self,
+                      forCellReuseIdentifier: self.cellId)
+        view.delegate = self
+        view.dataSource = self
+        return view
+    }()
+    
+    //MARK:- Methods
+    open func deselect()
+    {
+        guard let selected = self.table.indexPathForSelectedRow else {
+            return  //do nothing.
+        }
+        self.table.deselectRow(at: selected, animated: true)
+    }
+
+
+    //MARK:- Life cycle events
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(self.searchBar)
+        self.view.addSubview(self.table)
+    }
+    
+    open override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        self.searchBar.frame = CGRect(x: 0,
+                                      y: 0,
+                                      width: self.view.frame.width,
+                                      height: self.searchBarHeight)
+        self.table.frame = CGRect(x: 0,
+                                  y: self.searchBar.frame.height,
+                                  width: self.view.frame.width,
+                                  height: self.view.frame.height - self.searchBar.frame.height)
+    }
+    
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.deselect()
+    }
+
+    //MARK:- UITableViewDataSource
+    open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let viewModel = self.dataSource else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: self.cellId,
+                                                     for: indexPath as IndexPath)
+            cell.textLabel?.text = "Fail to load this cell."
+            return cell
+        }
+        return viewModel.tableView(cellForRowAt: indexPath, createdBy: self)
+    }
+
+    open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataSource?.tableView(numberOfRowsInSection: section) ?? 0
+    }
+    
+    open func numberOfSections(in tableView: UITableView) -> Int {
+        return self.dataSource?.numberOfSections?() ?? 1
+    }
+    
+    open func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.dataSource?.tableView?(titleForHeaderInSection: section) ?? nil
+    }
+    
+    //MARK:- UITableViewDelegate
+    open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let viewModel = self.dataSource else {
+            return  //do nothing
+        }
+
+        self.delegate?.tableView(didSelectRowAt: indexPath, from: viewModel)
+    }
+    
+    //MARK:- UISearchBarDelegate
+    open func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String)
+    {
+        self.dataSource?.filter(by: searchText)
+        self.table.reloadData()
+        return
+    }
+    
+    //MARK:- TableViewCellFactory
+    open func createCell(for indexPath: IndexPath) -> UITableViewCell {
+        return self.table.dequeueReusableCell(withIdentifier: self.cellId,
+                                              for: indexPath)
+    }
+    
+    //MARK:- Privates
+    private let cellId = String(describing: type(of: HDFilterableTableViewController.self))
+}
