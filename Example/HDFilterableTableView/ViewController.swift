fc1787fb82a66524fb4737ea5a244f627546509f
IgnorantCoder <fr052123@gmail.com>
2018-01-10 22:45:10 +0900

    add frame setter.

--- Example/HDFilterableTableView/ViewController.swift
+++ Example/HDFilterableTableView/ViewController.swift
@@ -18,10 +18,6 @@ class ViewController: HDFilterableTableViewController, UITableViewDelegate {
         return component
     }()
     
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -37,6 +33,10 @@ class ViewController: HDFilterableTableViewController, UITableViewDelegate {
         super.viewWillLayoutSubviews()
         
         self.view.frame = UIScreen.main.bounds
+        self.searchBarY
+            = UIApplication.shared.statusBarFrame.height
+            + (self.navigationController?.navigationBar.frame.height ?? 0)
+        self.searchBarHeight = 50
         self.button.frame = CGRect(x: self.view.frame.width - 60,
                                    y: self.view.frame.height - 60,
                                    width: 45,
